6f68be8f27ee8aafbce81e8f702219b4d6757a26
danglotb <bdanglot@gmail.com>
2020-09-08 15:49:33 +0200

    chore: play with the pom to try out the Mojo

--- src/main/java/org/powerapi/jjoules/Fibonacci.java
+++ src/main/java/org/powerapi/jjoules/Fibonacci.java
@@ -1,14 +1,14 @@
 package org.powerapi.jjoules;
 
 public class Fibonacci {
-	
+
 	public static final Fibonacci FIB = new Fibonacci();
-	
+
     public long fibo(int n) {
         return n < 2 ? n : fibo(n - 1) + fibo(n - 2);
     }
-    
+
     public static void test() {
-    	
+
     }
 }
